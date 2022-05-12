fb0619b6247a0a477a4528a3e113049fd06d68f1
plokij11 <97691110+plokij11@users.noreply.github.com>
2022-05-12 15:32:22 +0300

    Update Kazistov.js

--- Kazistov.js
+++ Kazistov.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
 const web3 = new Web3('https://ropsten.infura.io/v3/139ff4aadbf34e68bd589f0d3b9a3233')
 
 const 
-    privateKey = '96e896bb72e59e580c8cc95ceb0b1169a61d2af00293747a4af1d897b4379df7',
+    privateKey = '',
     publicKey = '0x5289321A7A331Fb98dcc85C61302e9fAdb0e7708',
     receiverPublicKey = '0xc53D6C0148ddC28Efe623Ab3aD54da5C7779b25C';
 
